0b46d8749aac521d8baa3774dee875762247bb48
DESKTOP-K10ROJB\Gab <gforget2@gmail.com>
2017-07-08 15:10:07 -0400

    controler qui marche sti

--- Assets/Script/CharacterController/RunnerController.cs
+++ Assets/Script/CharacterController/RunnerController.cs
@@ -16,6 +16,7 @@ public class RunnerController : MonoBehaviour {
     Rigidbody m_Rigidbody;
     Animator m_Animator;
     CapsuleCollider m_Capsule;
+    CharacterController controller;
 
     private GamePadState state;
     private GamePadState prevState;
@@ -47,49 +48,53 @@ public class RunnerController : MonoBehaviour {
     // Use this for initialization
     void Awake ()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        //m_Rigidbody = GetComponent<Rigidbody>();
         m_Capsule = GetComponent<CapsuleCollider>();
 
         m_CapsuleHeight = m_Capsule.height;
         m_CapsuleCenter = m_Capsule.center;
 
         gamepad = GamepadManager.Instance.GetGamepad(playerIndex);
+
+        controller = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        if (gamepad.GetButtonDown("A") && isGrounded)
+        if (gamepad.GetButtonDown("A") && controller.isGrounded)
         {
             yVelocity = JumpInitialVelocity;
         }
     }
 
-	// Update is called once per frame
-	void FixedUpdate ()
+    // Update is called once per frame
+    void FixedUpdate()
     {
         if (!gamepad.IsConnected) return;
+
+        Debug.Log(controller.isGrounded);
 
         xVelocity = gamepad.GetStick_L().X * Speed;
         yVelocity -= gravity;
 
-        Vector3 RaycastStartPosition = transform.position - new Vector3(0f, (m_Capsule.height / 2), 0f);
-
-        RaycastHit hit;
-        Debug.DrawLine(RaycastStartPosition, RaycastStartPosition + (transform.up*-1f) * groundDistance);
-
-        if (Physics.Raycast(RaycastStartPosition, transform.up * -1f, out hit, groundDistance, layermask) && yVelocity <= 0)
-        {
-            isGrounded = true;
-            yVelocity = 0;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-
-        Debug.Log(yVelocity);
         direction = (transform.right * xVelocity) + (transform.up*yVelocity);
-        m_Rigidbody.velocity = direction * Time.deltaTime;
+        controller.Move(direction*Time.deltaTime);
+
+        //m_Rigidbody.velocity = direction * Time.deltaTime;
+
+        //if (Physics.Raycast(RaycastStartPosition, transform.up * -1f, out hit, groundDistance) && yVelocity <= 0)
+        //{
+        //    isGrounded = true;
+        //    yVelocity = 0;
+        //}
+        //else
+        //{
+        //    isGrounded = false;
+        //}
+
+        //Debug.Log(yVelocity);
+        //direction = (transform.right * xVelocity) + (transform.up*yVelocity);
+        //m_Rigidbody.velocity = direction * Time.deltaTime;
     }
 
 }
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -202,6 +202,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		void CheckGroundStatus()
 		{
 			RaycastHit hitInfo;
+
 #if UNITY_EDITOR
 			// helper to visualise the ground check ray in the scene view
 			Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
