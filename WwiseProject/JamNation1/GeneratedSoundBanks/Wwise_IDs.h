05a6814ab42264b1d63c5fcf0856472bb80bd9df
Jean Gravel <jean.gravel2@yahoo.com>
2017-07-08 22:50:05 -0400

    New Soundbank 22h49

--- WwiseProject/JamNation1/GeneratedSoundBanks/Wwise_IDs.h
+++ WwiseProject/JamNation1/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,6 +17,14 @@ namespace AK
         static const AkUniqueID CAVE_BEACH = 1201886138U;
         static const AkUniqueID JUNGLE_CAVE = 2566409294U;
         static const AkUniqueID MOUNTAIN_JUNGLE = 3833726988U;
+        static const AkUniqueID P1_DYING = 3419501818U;
+        static const AkUniqueID P1_JUMP = 150832419U;
+        static const AkUniqueID P1_OOPS = 1912722926U;
+        static const AkUniqueID P1_THROW = 1022741657U;
+        static const AkUniqueID P2_DYING = 104172609U;
+        static const AkUniqueID P2_JUMP = 2155174042U;
+        static const AkUniqueID P2_OOPS = 1372435655U;
+        static const AkUniqueID P2_THROW = 1424313346U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
@@ -41,9 +49,18 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMB = 1117531639U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIQUE = 69765534U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID VO = 1534528548U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
 
 }// namespace AK
 
